7f104c06570235bd6c9b2dea6b2bd4aa0f13061f
NavaneethaBonala <149959297+NavaneethaBonala@users.noreply.github.com>
2025-04-16 21:44:09 +0530

    3rd assignment

---
+++ Assignments/assignments03/Assignment03.java
@@ -0,0 +1,25 @@
+package assignments03;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+
+public class Assignment03 {
+	
+	public static void main(String[] args) {
+		
+		ChromeDriver driver = new ChromeDriver();
+		FirefoxDriver driver1 = new FirefoxDriver();
+		InternetExplorerDriver driver2 = new InternetExplorerDriver();
+		EdgeDriver driver3 = new EdgeDriver();
+		SafariDriver driver4 = new SafariDriver();
+		
+		
+		
+		
+	}
+
+}
